697abfe9873c653e67720c78a9d31d666c6f6fad
Yax <kianby@madyanne.fr>
2015-04-25 16:42:02 +0200

    Pull out email mngt code from Pecosys project

---
+++ app/srmail/__init__.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import json
+import logging
+from flask import Flask
+from werkzeug.contrib.fixers import ProxyFix
+from srmail import scanemail
+
+app = Flask(__name__)
+
+
+def load_config():
+    """ load global config from json file
+    """
+    config_path = os.environ['CONFIG_PATHNAME']
+    logger.info("Load config from %s" % config_path)
+    with open(config_path, 'rt') as config_file:
+        config = json.loads(config_file.read())
+        return config
+
+
+def configure_logging(level):
+
+    logger.setLevel(level)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+
+
+def get_entire_config():
+    return config
+
+
+def get_config(section, param, value=None):
+    try:
+        value = config[section][param]
+    except:
+        logger.warn("missing config param %s.%s" % (section, param))
+    return value
+
+# configure logging
+logger = logging.getLogger(__name__)
+configure_logging(logging.DEBUG)
+
+# configure flask
+
+config = load_config()
+config["global"]["cwd"] = os.getcwd()
+app.config["app"] = config
+
+# start email scanner thread
+scanemail.start(config)
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+logger.info("Start SRMAIL application")
---
+++ app/srmail/scanemail.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import time
+from threading import Thread
+import logging
+import re
+from srmail import imap
+
+logger = logging.getLogger(__name__)
+
+
+class EmailScanner(Thread):
+
+    def __init__(self, config):
+        super(EmailScanner, self).__init__()
+        self.app_config = config
+
+
+    def stop(self):
+        logger.info("stop requested")
+        self.is_running = False
+
+    def run(self):
+
+        self.is_running = True
+
+        while self.is_running:
+
+            try:
+                with imap.Mailbox(self.app_config) as mbox:
+                    count = mbox.get_count()
+                    # logger.debug('check inbox: %d email(s)' % count)
+                    for num in range(count):
+                        msg_num = num + 1
+                        msg = mbox.fetch_simple_message(msg_num)
+                        process(mbox, msg_num, msg)
+                        break
+
+            except:
+                logger.exception("main loop exception")
+
+            # check email every 30 seconds
+            # TODO make configurable
+            time.sleep(300)
+
+        self.is_running = False
+
+
+def process(mbox, msg_num, msg):
+
+    # log message
+    logger.info('%s Message %d %s' % ('-' * 30, msg_num, '-' * 30))
+    for key in msg.keys():
+        logger.info('%s = %s' % (key, msg[key]))
+
+    msg['type'] = 'reply_comment_email'
+    # TODO notify listeners
+    #processor.enqueue(msg)
+
+    # delete message
+    #mbox.delete_message(msg_num)
+
+
+def start(config):
+    scanner = EmailScanner(config)
+    scanner.start()
